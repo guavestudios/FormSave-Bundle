9654bd9e9f23526916b5435baecdebe8b4f116eb
Christopher Stadler <christopher.stadler@guavestudios.com>
2024-07-11 16:58:29 +0200

    feature: start testing for contao 5+

--- src/Controller/ContentElement/FormNameController.php
+++ src/Controller/ContentElement/FormNameController.php
@@ -8,19 +8,15 @@ use Contao\BackendTemplate;
 use Contao\ContentModel;
 use Contao\CoreBundle\Controller\ContentElement\AbstractContentElementController;
 use Contao\CoreBundle\DependencyInjection\Attribute\AsContentElement;
-use Contao\CoreBundle\ServiceAnnotation\ContentElement;
+use Contao\CoreBundle\Twig\FragmentTemplate;
 use Contao\System;
-use Contao\Template;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
-/**
- * @ContentElement("formName", category="includes", template="content_element/form-name")
- */
 #[AsContentElement('formName', category: 'includes', template: 'content_element/form-name')]
 class FormNameController extends AbstractContentElementController
 {
-    protected function getResponse(Template $template, ContentModel $model, Request $request): Response|null
+    protected function getResponse(FragmentTemplate $template, ContentModel $model, Request $request): Response
     {
         if (System::getContainer()->get('contao.routing.scope_matcher')->isBackendRequest($request)) {
             $template = new BackendTemplate('be_wildcard');
